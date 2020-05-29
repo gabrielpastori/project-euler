6c028288927f9ab2ed5208ba5b45a3c08fd761a0
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-05-28 23:43:09 -0300

    Add Problem 5 - Smallest multiple

---
+++ Cpp/p5.cpp
@@ -0,0 +1,26 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+*/
+#include <iostream>
+#define ulli unsigned long long int
+using namespace std;
+
+ulli gdc(ulli a,ulli b){
+    return (b==0? a: gdc(b,a%b));
+}
+ulli lcm(ulli a,ulli b){
+    return (a*b)/(gdc(a,b));
+}
+int main(){
+    ulli result=1;
+    for(int i=1;i<=20;i++){
+        result=lcm(i,result);
+    }
+    cout<<result<<endl;
+}
+
+// gdc : Greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
+// lcm : Least common divisor (https://en.wikipedia.org/wiki/Least_common_multiple)
